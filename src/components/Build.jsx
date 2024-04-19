27f44691297b14d4ee6e7a29b96831a479c339b7
Mona Lisa <surajkmr20167@gmail.com>
2024-04-19 09:03:15 +0530

    update build screen

--- src/components/Build.jsx
+++ src/components/Build.jsx
@@ -4,6 +4,8 @@ import Sidebar from './Sidebar'
 import { FaClockRotateLeft } from 'react-icons/fa6';
 import { BsThreeDots } from "react-icons/bs";
 import { MdOutlineCloudUpload } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import tempImage from '../assets/imageData2.png';
 
 const Build = () => {
     return (
@@ -11,7 +13,7 @@ const Build = () => {
             <Header />
             <div className='flex'>
                 <Sidebar />
-                <div className='flex-1'>
+                <div className='flex-1 '>
                     <div className='flex-1 flex  h-16 '>
                         <div className='ml-10 mt-8 text-3xl font-medium flex-1'>Build</div>
                         <div className='mt-10 mr-10 flex space-x-4 justify-content items-center'>
@@ -29,10 +31,57 @@ const Build = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='m-32'>Images</div>
+                    <div className='ml-10 mt-8 flex'>
+                        <button className='rounded border-2 w-16 border-gray-300 mr-4'>Filter</button>
+                        <button className='rounded border-2 w-16 border-gray-300 mr-6'>Sort</button>
+                        <div className='flex justify-center items-center mr-4'>
+                            <input type="checkbox" id='groundTruth' className='size-4 mr-2' />
+                            <label htmlFor="groundTruth">Ground Truth</label>
+                        </div>
+                        <div className='flex justify-center items-center'>
+                            <input type="checkbox" id='prediction' className='size-4 mr-2' />
+                            <label htmlFor="prediction">Prediction</label>
+                        </div>
+                    </div>
+                    <div className='ml-10    flex flex-wrap'>
+                        <img src={tempImage} alt="" className='flex h-48 rounded mr-6 mt-6 hover:outline hover:outline-offset-4' />
+                        <img src={tempImage} alt="" className='flex h-48 rounded mr-6 mt-6 hover:outline hover:outline-offset-4' />
+                        <img src={tempImage} alt="" className='flex h-48 rounded mr-6 mt-6 hover:outline hover:outline-offset-4' />
+                        <img src={tempImage} alt="" className='flex h-48 rounded mr-6 mt-6 hover:outline hover:outline-offset-4' />
+                        <img src={tempImage} alt="" className='flex h-48 rounded mr-6 mt-6 hover:outline hover:outline-offset-4' />
+                        <img src={tempImage} alt="" className='flex h-48 rounded mr-6 mt-6 hover:outline hover:outline-offset-4' />
+                        <img src={tempImage} alt="" className='flex h-48 rounded mr-6 mt-6 hover:outline hover:outline-offset-4' />
+                    </div>
                 </div>
-                <div className='border w-96 '>
-                    chart
+                <div className='border-2 border-gray-300 w-96 '>
+                    <div className='flex justify-between m-4 mb-2 text-gray-500'>
+                        <h2>Model</h2>
+                        <IoClose />
+                    </div>
+                    <div className='flex justify-between ml-4 mr-4'>
+                        <h2 className='font-medium'>Model-01-30-2024_1</h2>
+                        <BsThreeDots />
+                    </div>
+                    <div className='border border-black mt-2 h-96 flex flex-col justify-between'>
+                        <h2>Graph</h2>
+                        <div className='flex justify-end mr-10'>
+                            <button className='m-2 rounded border-2 w-20 border-blue-400 text-blue-900'>Deploy</button>
+                            <button className='m-2 rounded border-2 w-20 border-blue-400 bg-blue-700 text-white'>Predict</button>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className='m-2 text-gray-600 font-medium'>Performance</h2>
+                        <div className='flex justify-center items-center space-x-20 mt-8'>
+                            <div>
+                                <h2 className='font-bold text-3xl'>83%</h2>
+                                <h3 className='text-gray-600'>Precision</h3>
+                            </div>
+                            <div>
+                                <h2 className='font-bold text-3xl'>88%</h2>
+                                <h3 className='text-gray-600'>Recall</h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
